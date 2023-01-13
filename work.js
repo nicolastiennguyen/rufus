7a1d13f9c9bf742c76ab0c52b589d6173328ce5c
Nick <nicolastiennguyen@gmail.com>
2023-01-12 20:46:45 -0800

    installed gitkit

---
+++ work.js
@@ -0,0 +1,10 @@
+// import { simpleGit, CleanOptions } from 'simple-git';
+
+// simpleGit().clean(CleanOptions.FORCE);
+
+// const git = simpleGit();
+// try {
+//   await console.log(git.branchLocal())
+// } catch (e) {
+//    console.log(e, 'ERROR')
+// }
