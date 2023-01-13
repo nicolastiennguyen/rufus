6ea86b7ab5b7ef4e74fa67719a048eebc6abee5f
Nick <nicolastiennguyen@gmail.com>
2023-01-12 21:09:27 -0800

    test

--- work.js
+++ work.js
@@ -1,25 +1,27 @@
 import { exec } from "child_process"
 
 // prints active branch
-exec('git rev-parse --abbrev-ref HEAD', (err, stdout) => {
+exec('git rev-parse --abbrev-ref HEAD', (err, res) => {
     if (err) {
         console.log(err)
     }
 
-    if (typeof stdout === 'string') {
-      console.log('active branch:', stdout)
+    if (typeof res === 'string') {
+      console.log('active branch:', res)
     }
 });
 
 // prints whether repository files have been modified
-exec('git status', (err, stdout) => {
+exec('git status', (err, res) => {
     if (err) {
         console.log(err)
     }
 
-    if (typeof stdout === 'string') {
+    if (typeof res === 'string') {
         console.log('local changes:', true)
+        console.log(res, 'res TRUE')
     } else {
         console.log('local changes:', false)
+        console.log(res, 'res FALSE')
     }
 });
