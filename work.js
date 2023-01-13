7571697ea5c14b30a2694299f67561f3c5a7390e
Nick <nicolastiennguyen@gmail.com>
2023-01-12 20:48:35 -0800

    gitkit

--- work.js
+++ work.js
@@ -8,3 +8,16 @@
 // } catch (e) {
 //    console.log(e, 'ERROR')
 // }
+
+
+import GitKit from 'gitkit';
+import NativeFS from 'gitkit/lib/fs/native';
+
+// Prepare the filesystem
+var fs = NativeFS(process.cwd());
+
+// Create a repository instance
+var repo = GitKit.Repository.createWithFS(fs, isBare);
+
+GitKit.BranchUtils.list(repo)
+    .then(function(branches) { console.log(branches) })
