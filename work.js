78fc2fc153b8de24775b2dd3d9aa52da09ea8c6d
Nick <nicolastiennguyen@gmail.com>
2023-01-12 21:27:42 -0800

    prints whether the current head commit was authored in the last week

--- work.js
+++ work.js
@@ -2,20 +2,19 @@ import { exec } from "child_process"
 
 // prints active branch
 exec('git rev-parse --abbrev-ref HEAD', (err, res) => {
-    if (err) {
-        console.log(err)
-    }
-
-    if (typeof res === 'string') {
-      console.log('active branch:', res)
-    }
+    if (err) console.log(err)
+    res ? console.log('active branch:', res) : null
 });
 
 // prints whether repository files have been modified
 exec('git status --porcelain=v1', (err, res) => {
-    if (err) {
-        console.log(err)
-    }
+    if (err) console.log(err)
 
     res ? console.log('local changes: True') : console.log('local changes: False')
 });
+
+// prints whether the current head commit was authored in the last week
+exec("git log --since '1 week ago'", (err, res) => {
+    if (err) console.log(err)
+    res ? console.log('recent commit: True') : console.log('recent commit: False')
+})
